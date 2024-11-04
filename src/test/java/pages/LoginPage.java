7e1c2d97458360a207f61a906c9d98a2516ebe08
tarikaziz0933 <tarikaziz0933@gmail.com>
2024-11-04 23:10:36 +0600

    Your commit message describing the update

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,52 @@
+package pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "email")
+    WebElement txtEmail;
+
+    @FindBy(id = "password")
+    WebElement txtPassword;
+
+    @FindBy(className = "MuiButton-containedPrimary")
+    private WebElement loginButton;
+
+
+    @FindBy(linkText = "Reset it here")
+    public WebElement resetLink;
+
+
+
+    public void dologin(String email, String password)
+    {
+        txtEmail.sendKeys(email);
+        txtPassword.sendKeys(password);
+        loginButton.click();
+    }
+
+    public void clearCreds()
+    {
+        txtEmail.sendKeys(Keys.CONTROL,"a");
+        txtEmail.sendKeys(Keys.BACK_SPACE);
+        txtPassword.sendKeys(Keys.CONTROL,"a");
+        txtPassword.sendKeys(Keys.BACK_SPACE);
+    }
+
+    public LoginPage (WebDriver driver)
+    {
+        PageFactory.initElements(driver,this);
+    }
+
+
+
+
+
+
+
+}
